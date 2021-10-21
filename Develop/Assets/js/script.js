f2a16a69e95f45860fa7bf0dfdccbeaf53a11b36
Ami Asokumar <gasokumar@gmail.com>
2021-10-21 01:18:35 -0400

    Checked all files and started javascript. Familiarized myself with moment

---
+++ Develop/Assets/js/script.js
@@ -0,0 +1,6 @@
+var currentDay = document.querySelector("#currentDay");
+var timeBlocks = document.querySelector(".container");
+
+var nowMoment = moment();
+currentDay.innerHTML = "This is now linked";
+// currentDay.innerHTML = nowMoment;
