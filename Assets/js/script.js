bbe85ade9f6d6194eff33e2c9d19c4dc6786c6d2
Ami Asokumar <gasokumar@gmail.com>
2021-10-28 02:50:53 -0400

    refactored some code as well as added comments describing what each line does

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,31 +1,15 @@
 $(document).ready(function () {
-  //DEPENDENCIES ===========================================
-  var saveBtns = document.getElementsByClassName("saveBtn");
-  var description = document.querySelector(".description");
+  //DEPENDENCIES ====================================================
   var currentDay = document.querySelector("#currentDay");
-  var timeBlocks = document.querySelector(".container");
-  var rows = document.getElementsByClassName("row");
 
-  //DATA =========================
+  //DATA =============================================================
 
   // Setting the day, month, and date at the top of the screen
   var today = moment().format("[Today is] dddd, MMMM Do");
   currentDay.innerHTML = today;
 
-  //FUNCTIONS =====================
-
-  //Save Function
-  // Used jQuery to get each block by their id, and the selects their description (text box). Then puts that description in local storage
-  //Selects save button, then on click
-  $(".saveBtn").on("click", function () {
-    var rowHour = $(this).parent().attr("id"); // Gets id from parent of saveBtn (where row id that tells you the hour of the box is)
-    var description = $(this).siblings(".description").val();
-
-    localStorage.setItem(rowHour, description);
-  });
-
   // Loads the locaStorage items into their respective description boxes upon reload
-
+  //Selects item by going to id then description, then puts in relevant stored data
   $("#9 .description").val(localStorage.getItem("9"));
   $("#10 .description").val(localStorage.getItem("10"));
   $("#11 .description").val(localStorage.getItem("11"));
@@ -36,6 +20,17 @@ $(document).ready(function () {
   $("#16 .description").val(localStorage.getItem("16"));
   $("#17 .description").val(localStorage.getItem("17"));
 
+  //FUNCTIONS =========================================================
+
+  //Save Function
+  // Used jQuery to get each block by their id, and the selects their description (text box). Then puts that description in local storage
+  //Selects save button, then on click
+  $(".saveBtn").on("click", function () {
+    var rowHour = $(this).parent().attr("id"); // Gets id from parent of saveBtn (div that holds id for respective hour)
+    var description = $(this).siblings(".description").val(); //Gets description from sibling of button (description box)
+    localStorage.setItem(rowHour, description); // Puts the key:value of rowHour: description into local storage when the save button is clicked.
+  });
+
   // Changes classes depending on the block's relation to the current hour, resulting in a color change depending on whether the block is in the past, present, or future.
   // This function is responsible for the color change for each block
   function hourTracker() {
@@ -44,7 +39,7 @@ $(document).ready(function () {
     // Used jQuery to select each block (each day has a box/row and has a class of time.block)
     //For each time block
     $(".time-block").each(function () {
-      // Set a variable that sets the id of each block (id = to the hour of the day) to an integer
+      // Set a variable that sets the id of each block (id = to the hour of the day) to an integer. ParseInt ensures an integer value is stored.
       var blockHour = parseInt($(this).attr("id"));
       // If that integer is lower than the current hour, adds the class "past" along with its associated css to the block
       if (blockHour < currentHour) {
